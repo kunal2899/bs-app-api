f34566762adeedc6abd8f867e4709b12cd113c48
kunal2899 <kunaljain649@gmail.com>
2022-10-15 02:22:11 +0530

    Updated database config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,30 +1,14 @@
 const { Pool } = require("pg");
 require("dotenv/config");
 
-const currEnvironment = process.env.CURRENT_ENVIRONMENT || "development";
-const { [currEnvironment]: config } = require("./config.json");
-
-let pool;
+const currEnvironment = process.env.NODE_ENV || "development";
+let { [currEnvironment]: config } = require("./config.json");
 
 if ("env_var" in config) {
-  const {
-    username: uname,
-    password: pass,
-    host: hst,
-    port: prt,
-    database: db,
-  } = config.env_var;
-  const {
-    [uname]: user,
-    [pass]: password,
-    [hst]: host,
-    [prt]: port,
-    [db]: database,
-  } = process.env;
-  pool = new Pool({ user, password, host, database, port });
-} else {
-  const { username: user, password, host, port, database } = config;
-  pool = new Pool({ user, password, host, database, port });
+  const { [config.env_var]: connectionString } = process.env;
+  config = { connectionString };
 }
+
+const pool = new Pool(config);
 
 module.exports = pool;
